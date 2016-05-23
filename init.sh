372f8891195fb7c70d533caa740f6743c33ab8ac
Alexey Vetkhov <vethov.alexey@gmail.com>
2016-05-23 16:28:34 +0300

    Change gunicorn config. Change init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ask.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-cd /home/box/web/etc
-sudo gunicorn -c gunicorn.qa
+sudo ln -sf /home/box/web/etc/gunicorn.qa /etc/gunicorn.d/ask
+sudo /etc/init.d/gunicorn restart

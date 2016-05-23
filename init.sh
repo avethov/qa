b766ffc12e193bc741206de83b79d6606512c4dc
Alexey Vetkhov <vethov.alexey@gmail.com>
2016-05-23 16:30:02 +0300

    Change init.sh

--- init.sh
+++ init.sh
@@ -4,4 +4,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ask.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.qa /etc/gunicorn.d/ask
+sudo rm -rf /etc/gunicorn.d/*.example
 sudo /etc/init.d/gunicorn restart

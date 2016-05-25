cce9e0fdb3aed18b7c86894c55e17a65d0b4e0b6
Alexey Vetkhov <vethov.alexey@gmail.com>
2016-05-25 14:21:31 +0300

    Change init.sh. Fix models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,26 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class QuestionManager(models.Model):
+    def new(self):
+        pass
+
+    def popular(self):
+        pass
+
+
 class Question(models.Model):
-    title = models.CharField
-    text = models.TextField
-    added_at = models.DateField
-    rating = models.IntegerField
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField(null=True)
     author = models.OneToOneField(User, related_name="user_question_related")
     likes = models.ManyToManyField(User)
     objects = QuestionManager()
 
 
 class Answer(models.Model):
-    text = models.TextField
-    added_at = models.DateField
+    text = models.TextField()
+    added_at = models.DateField()
     question = models.ForeignKey(Question)
     author = models.OneToOneField(User)
-
-
-class QuestionManager(models.Model):
-    def new(self):
-        pass
-    
-    def popular(self):
-        pass
--- etc/stepic.sql
+++ etc/stepic.sql
@@ -1,4 +1,4 @@
-create database if not exists ask charset = 'utf-8';
-create user if not exists 'stepic'@'localhost' identified by 'stepic';
+create database if not exists ask;
+create user 'stepic'@'localhost' identified by 'stepic';
 grant all privileges on ask.* to 'stepic'@'localhost';
 flush privileges;

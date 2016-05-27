37bbf578ac2fccb5691cbb5600e8c323fe16aa23
vethov.alexey <vethov.alexey@gmail.com>
2016-05-27 11:46:28 +0000

    Add examples config files for gunicorn 17.5

--- init.sh
+++ init.sh
@@ -6,7 +6,3 @@ sudo /etc/init.d/nginx restart
 sudo /etc/init.d/mysql start
 mysql -uroot < /home/box/web/etc/stepic.sql
 /home/box/web/ask/manage.py syncdb
-sudo ln -sf /home/box/web/etc/gunicorn.qa /etc/gunicorn.d/ask
-sudo rm -rf /etc/gunicorn.d/*.example
-cd ./ask
-gunicorn --bind 0.0.0.0:8000 ask.wsgi 
